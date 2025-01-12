9c8da44672dac8f735b1c2a75427aab662fbe8cc
AbhiRamMaddali <abhiram.maddali1204@gmail.com>
2025-01-12 12:35:49 +0530

    Delete edge detection.py

--- edge detection.py
+++
@@ -1,6 +0,0 @@
-import cv2
-img=cv2.imread(r"C:\Users\abhir\Desktop\PXL_20210509_052249401.jpg")
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-canny=cv2.Canny(gray,150,175)
-cv2.imwrite(r"C:\Users\abhir\Desktop\P.jpg",canny)
-cv2.waitKey(0)
